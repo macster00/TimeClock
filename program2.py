38b90966161783c4fd4cfd6016d16c90efa9498f
macster00 <dmcalister00@gmail.com>
2016-01-24 20:15:32 -0500

    third commit

--- program2.py
+++ program2.py
@@ -1,15 +1,21 @@
-import printEmp
+import os
+import shutil
 class Prog2:
     def func1_test(self):
         print "function 1"
+        if not os.path.exists('J:\NewPythonFolder'):
+            os.makedirs('J:\NewPythonFolder')
+            
     
     
     def func2_test(self):
         print "function 2"
+        shutil.copytree("J:\NewPythonFolder", "J:\New")
         
 def main():
     p = Prog2()
        
-    printEmp.main()
+    #p.func1_test()
+    p.func2_test()
     
 main()
